210008fa73b038c86798e59efc4dc3285dd3994f
ZakMos <z.zza@hotmail.com>
2020-07-14 09:57:16 +0200

    added authors routes with view

---
+++ routes/authors.js
@@ -0,0 +1,37 @@
+// @ts-nocheck
+const express = require('express')
+const { default: author } = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+
+// All Authors Route
+router.get('/', (req, res) => {
+    res.render('authors/index')
+})
+
+// New Author Route
+
+router.get('/new', (req, res) => {
+    res.render('authors/new', {author: new Author()})
+})
+
+
+// Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render('authors/new', {
+            author: author, 
+            errorMessage: 'Error creating Author'
+        })
+    }
+})
+
+
+module.exports = router
