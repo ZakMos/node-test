2e8e690955d615d4d91b61fe75a913eb7b2f5918
zakmos <z.zza@hotmail.com>
2020-11-18 20:09:06 +0100

    Edit Author

--- routes/authors.js
+++ routes/authors.js
@@ -31,8 +31,7 @@ router.post('/', async (req, res) => {
   })
   try {
     const newAuthor = await author.save()
-    // res.redirect(`authors/${newAuthor.id}`)
-    res.redirect(`authors`)
+    res.redirect(`authors/${newAuthor.id}`)
   } catch {
     res.render('authors/new', {
       author: author,
@@ -45,12 +44,33 @@ router.get('/:id', (req, res) => {
   res.send('Show Author ' + req.params.id)
 })
 
-router.get('/:id/edit', (req, res) => {
-  res.send('Edit Author: ' + req.params.id)
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch { 
+    res.redirect('/authors')
+  }
 })
 
-router.put('/:id', (req, res) => {
-  res.send('Update Author', + req.params.id)
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+        res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error Updating Author'
+      })
+    }
+    
+  }
 })
 
 router.delete('/:id', (req, res) => {
